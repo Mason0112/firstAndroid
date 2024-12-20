72bbce88c1bcfe476236eb5e83ec0b8c21a33e82
DESKTOP-NELQMJ0\Mason <willy55406@gmail.com>
2024-12-20 11:02:08 +0800

    first commit

---
+++ app/src/main/java/com/mason/bmi/Hello.kt
@@ -0,0 +1,15 @@
+package com.mason.bmi
+
+class Hello {
+}
+
+fun main() {
+    println("Hello Kotlin")
+    val age =20
+    println(age.inc())
+    println(age)
+    var name ="Mason"
+    println(name.get(0))
+    name ="SuperMason"
+    println(name)
+}
